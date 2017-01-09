383c695021fc61b4008c50903d616b815a5c45c2
sebastienmm <sebastien_meylan@orange.fr>
2017-01-09 22:10:21 +0100

    première fonction 2er essai

--- generateur_galerie.sh
+++ generateur_galerie.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 . verif_fichier_traite.sh 
-verif_dossier_traite 
+verif_dossier_traite $1
 
-. verif_image.sh
-verif_presence_image
+#. verif_image.sh
+#verif_presence_image $1
 
-. verif_exist_miniature.sh 
-verif_exist_miniature  
+#. verif_exist_miniature.sh 
+#verif_exist_miniature $1 
 
-. convert_images.sh
-convert_image
+#. convert_images.sh
+#convert_image $1
 
-t./generateur_entete_html.sh
+#./generateur_entete_html.sh
 #./generateur_corps_html.sh
 #./generateur_pied_html.sh
 #./generateur_css.sh
