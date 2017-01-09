961cbeae4591d25467f81e1574cafe82e75998ac
sebastienmm <sebastien_meylan@orange.fr>
2017-01-09 15:44:49 +0100

    creation du dossier miniature dans le script convert_images.sh

--- convert_images.sh
+++ convert_images.sh
@@ -5,9 +5,11 @@
 
 convert_image(){
 	liste_fichier=$(`ls images`)
-
+	create_miniature=$(`mkdir miniatures`)
+	
 	for fichier in $liste_fichier
 	do
-		convert $fichier -resize 100x100 mini_$fichier
+		convert $fichier -resize 100x100 $fichier_mini
+		mv $fichier_mini /miniatures
 	done
 }
