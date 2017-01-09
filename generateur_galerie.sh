8db7bbc8dd68fab8c4c9b895621d158467861bb9
sebastienmm <sebastien_meylan@orange.fr>
2017-01-09 10:57:25 +0100

    Ajout du script de générateur de css et ajout des paramètres  et  aux scripts e vérification

--- generateur_galerie.sh
+++ generateur_galerie.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-./verif_fichier_traité.sh
-./verif_exist_miniature.sh
+./verif_fichier_traité.sh $1
+./verif_exist_miniature.sh $2
 ./convert_images.sh
 ./generateur_entete_html.sh
 ./generateur_corps_html.sh
 ./generateur_pied_html.sh
+./generateur_css.sh
 
