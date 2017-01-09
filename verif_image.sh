ebc26472484f537f9a425f0297ddcba7521442ae
sebastienmm <sebastien_meylan@orange.fr>
2017-01-09 15:09:14 +0100

    Ecriture de la fonction de verification de la présence de photo dans le dossier

---
+++ verif_image.sh
@@ -0,0 +1,33 @@
+
+# fonction permettant de vérifier la présence de fichier image dans le dossier entré 
+# en argument
+
+verif_presence_image(){
+	
+	compteur_image=0
+	tableau_format=('*.jpg' '*.png' '*.gif' '*.bmp')	
+	dossier_a_analise=$1	
+	liste_fichier=`ls $1`
+	sortie=`exit`
+
+	for fichier in $liste_fichier
+	do
+		for format in $tableau_format
+		do
+			if [ $fichier == $format ]
+			then
+				compteur_image= $compteur_image + 1
+			else
+				compteur_image= $compteur_image	
+			fi
+		done
+	done
+	if [ $compteur_image = 0 ]
+	then
+		echo " le dossier ne contient pas d'image "
+		echo " Vous devait choisir un nouveau dossier "
+		echo $sortie
+	else	
+		echo " Il y a $compteur_image dans le dossier "
+	fi
+}
