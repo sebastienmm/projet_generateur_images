2a9639a4e28bca0de405e96f09d58605df96a244
sebastienmm <sebastien_meylan@orange.fr>
2017-01-09 22:00:56 +0100

    modification du script principal appel des fonctions modif supplémentaire 1er essai

--- generateur_galerie.sh
+++ generateur_galerie.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 . verif_fichier_traite.sh 
-verif_dossier_traite $1
+verif_dossier_traite 
+
+. verif_image.sh
+verif_presence_image
+
 . verif_exist_miniature.sh 
-verif_exist_miniature $1
-. 
+verif_exist_miniature  
 
 . convert_images.sh
+convert_image
 
-
-#./generateur_entete_html.sh
+t./generateur_entete_html.sh
 #./generateur_corps_html.sh
 #./generateur_pied_html.sh
 #./generateur_css.sh
