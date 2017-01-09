813d44949f2dc190fe9cde5b970d4efcd4d43389
sebastienmm <sebastien_meylan@orange.fr>
2017-01-09 15:22:41 +0100

    Ecriture de la fonction verifiant l'existance d'un dossier miniature

---
+++ verif_exist_miniature.sh
@@ -0,0 +1,21 @@
+
+# fonction permettant de verifier l'existence d'un dossier nommé miniature dans le dossier traité
+
+verif_exist_miniature(){
+
+liste_fichiers=$(`ls $1`)
+nom_recherche="miniature"
+sortie=`exit`
+
+for fichier in liste_fichiers
+do
+	if [ $fichier == $nom_recherche ]
+		echo " Le fichier miniature existe déjà !! "
+	else
+		echo " La conversion des images en miniatures peut être réalisée "
+		. convert_images.sh
+		conversion_image 
+	fi
+done
+		
+}
