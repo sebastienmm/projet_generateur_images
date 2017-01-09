de56abb5206f908282017c0edfd4a4d7ccc5d282
sebastienmm <sebastien_meylan@orange.fr>
2017-01-09 14:35:44 +0100

    Ecriture de la fonction de vérification du premier argument du script generateur_galerie.sh : cet argument doit être présent et correspondre à un dossier existant

---
+++ verif_fichier_traite.sh
@@ -0,0 +1,20 @@
+
+# fonction permettant de verifier si le premier argument du générateur de galerie est renseigné et
+# si l'argument correspond à un dossier existant
+
+verif_dossier_traite() {
+	
+	dossier_a_traite=$1
+
+	if [ -z $dossier_a_traite ]
+	then
+		read -p "Entrez le chemin absolu du fichier à traité" dossier_a_traite 
+	else
+		if [ -e $dossier_a_traite ] && [ -d $dossier_a_traite ]
+		then
+			echo "le dossier existe"
+		else
+			echo " Le dossier n'existe pas "
+		fi
+	fi
+}
