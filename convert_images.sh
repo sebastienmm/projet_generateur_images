16611736495663ec5b730cdb843d86fe982d777b
sebastienmm <sebastien_meylan@orange.fr>
2017-01-10 15:09:30 +0100

    modification du fichier convert_image.sh

--- convert_images.sh
+++ convert_images.sh
@@ -5,11 +5,12 @@
 
 convert_image(){
 	liste_fichier=$(`ls images`)
-	create_miniature=$(`mkdir miniatures`)
+	mkdir miniatures
 	
 	for fichier in $liste_fichier
 	do
-		convert $fichier -resize 100x100 $fichier_mini
-		mv $fichier_mini /miniatures
+		miniphoto="mini_$fichier"
+		convert $fichier -resize 100x100 "$miniphoto"
+		mv $miniphoto /miniatures
 	done
 }
