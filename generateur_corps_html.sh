6a9a622a11b2d60f7992cbaa89b3a12f92b08026
sebastienmm <sebastien_meylan@orange.fr>
2017-01-11 16:11:44 +0100

    Mise en forme du html

--- generateur_corps_html.sh
+++ generateur_corps_html.sh
@@ -4,6 +4,10 @@ image_a_afficher=`ls miniatures`
 
 for image in $image_a_afficher
 do
-	echo "<p><a href=image/$image <img src=$image alt="$image" title="$image"/> </p><br/>" >> galerie.html
+	echo "<body>
+			<p>
+				<a href=images/$image <img src=$image alt="$image" title="$image"/>
+			</p><br/>
+		  </body>" >> galerie.html
 done
 
