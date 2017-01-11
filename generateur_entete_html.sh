27ca412bae5f6a722259e6dc48b5156f5c61c20c
sebastienmm <sebastien_meylan@orange.fr>
2017-01-11 15:47:40 +0100

    Entete html générée en bash

--- generateur_entete_html.sh
+++ generateur_entete_html.sh
@@ -1,6 +1,10 @@
-<!DOCTYPE html>
+#/bin/bash
+
+# Entete html généré en bash
+
+echo "!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="utf-8">
 		<title> Generateur d'image </title>
-	</head>
+	</head>"
