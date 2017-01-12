555dfc10580c2bab9a7feed3a78fc4f2a345240f
sebastienmm <sebastien_meylan@orange.fr>
2017-01-12 10:17:02 +0100

    Correction des différentes erreurs rencontrées dans le code html

--- generateur_entete_html.sh
+++ generateur_entete_html.sh
@@ -1,49 +1,30 @@
 #/bin/bash
 
 # Entete html généré en bash
-if [ $3 != "galerie.html" ]
-then
-	mkdir galerie.html
-	echo "!DOCTYPE html>
+	
+	
+	echo '' > galerie.html
+	echo "<!DOCTYPE html>
 			<html>
 				<head>
 					<meta charset="utf-8">
 				<title> Generateur d'image </title>
-				</head>" >> galerie.html
+				</head>" > galerie.html
 
 	# corps html généré
 
 	image_a_afficher=`ls miniatures`
-	echo " <body> "
+	echo "		 <body> " >> galerie.html
 	for image in $image_a_afficher
 	do
 		echo "
-				<p>
-					<a href=images/$image <img src=$image alt="$image" title="$image"/>
-				</p><br/>
+					<p>
+						<a href=images/$image> <img src=miniatures/$image alt="$image" title="$image"/></a>
+					</p><br/>
 		  	" >> galerie.html
 	done
-	echo " </body " >> galerie.html
-else
-	echo "!DOCTYPE html>
-			<html>
-				<head>
-					<meta charset="utf-8">
-					<title> Generateur d'image </title>
-				</head>" >> $3
+	echo " 		</body>
+			</html> " >> galerie.html
+	
 
-	# corps html généré
-		echo "	<body> ">> $3
-
-	image_a_afficher=`ls miniatures`
-
-	for image in $image_a_afficher
-	do
-		echo "			<p>
-						<a href=images/$image <img src=$image alt="$image" title="$image"/>
-					</p><br/>
-		  		" >> $3
-	done
-	echo  "</body> " >> $3
-fi
 
